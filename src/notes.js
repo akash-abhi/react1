cf51cf34ad073ec868904122edb4c25e1fbf789d
akash-abhi <akasheng07@gmail.com>
2021-07-28 16:15:32 +0530

    first deployment

---
+++ src/notes.js
@@ -0,0 +1,29 @@
+const notes = [
+    {
+        key:1,
+        title: "Delegation",
+        content:
+        "First content app."
+    },
+    {
+        key:2,
+        title:"Loops",
+        content:
+        "Second content details."
+    },
+    {
+        key:3,
+        title:"Arrays",
+        content:
+        "Third content details and extra "
+    },
+    {
+        key: 4,
+        title: "Linked Lists",
+        content:
+        "Easy way to add element and efficient also"
+    }
+
+]
+
+export default notes;
